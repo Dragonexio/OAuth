20a4a448547b11a64843ee364515041ecb33418f
DragonEx <service@dragonex.io>
2019-04-19 16:11:15 +0800

    add scopes enum

--- oauthgo/consts.go
+++ oauthgo/consts.go
@@ -3,11 +3,14 @@ package oauthgo
 // scopes
 const (
 	ScopeLogin       = 1
+	ScopeUserInfo    = 2
 	ScopeWithdraw    = 3
 	ScopeTrade       = 4
 	ScopePayUser2App = 5
 	ScopePayApp2User = 6
 	ScopeAssetInfo   = 7
+	ScopeOpenApi     = 8
+	ScopeAutoAuth    = 9
 )
 
 // transfer direction between user and app
